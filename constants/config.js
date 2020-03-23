c4bb9e4a8d6e66b45b4c6b8c140651a57e41641e
Denis Efremov <efremoz@gmail.com>
2020-03-23 14:48:45 +0300

    update config

--- constants/config.js
+++ constants/config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = isProduction ? process.env.JWT_SECRET : 'dev-secret';
 
-const DB_LINK = isProduction ? process.env.DB_LINK : 'mongodb://localhost:27017/newsdb';
+const DB_LINK = 'mongodb://localhost:27017/newsdb';
 const DB_OPTIONS = {
   useNewUrlParser: true,
   useCreateIndex: true,
